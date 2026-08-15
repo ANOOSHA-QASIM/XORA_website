8ac53238d02c0e281c18bf5cb85743560c3f48d4
anusha-qasim <anushatech333@gmail.com>
2026-08-15 15:39:21 +0500

    headline change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -404,7 +404,7 @@ export default function Hero() {
 
           <span className="mt-2 block overflow-hidden">
             <motion.span
-              className="inline-block text-[#c6ff00]"
+              className="inline-block text-[#c6ff00] italic font-light"
               initial={{
                 opacity: 0,
                 y: 90,
